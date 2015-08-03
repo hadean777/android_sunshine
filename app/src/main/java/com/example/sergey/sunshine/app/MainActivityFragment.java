aeb19f88f7b5e2db9a0148f47542325e259ed045
sergey <sergey@jessie>
2015-08-03 15:59:23 +0300

    Changes + docs

--- app/src/main/java/com/example/sergey/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/sergey/sunshine/app/MainActivityFragment.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,12 +35,20 @@ public class MainActivityFragment extends Fragment {
                 "Thurs - Asteroids - 75/65",
                 "Fri - Heavy Rain - 65/56",
                 "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
-                "Sun - Sunny - 80/68"
+                "Sun - Sunny - 80/68",
+                "One more",
+                "Other part of list",
+                "Other part of list 2",
+                "Other part of list 3",
+                "Other part of list 4",
+                "Other part of list 5",
+                "Other part of list 6",
+                "Other part of list 7"
         };
 
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
-        Adapter mForecastAdapter = new ArrayAdapter<String>(
+        ListAdapter mForecastAdapter = new ArrayAdapter<String>(
                 // The current context
                 getActivity(),
                 // ID of list item layout
@@ -47,6 +57,10 @@ public class MainActivityFragment extends Fragment {
                 R.id.list_item_forecast_textview,
                 // Forecast data
                 weekForecast);
+
+        // Get a reference to ListView and attach this adapter to ListView
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
 
 
         return rootView;
